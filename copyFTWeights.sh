9a500eee2a3414e303428b8e6eff2a028a7d11cc
willparker-and <william.parker@and.digital>
2023-11-23 16:52:00 +0000

    *

--- copyFTWeights.sh
+++ copyFTWeights.sh
@@ -4,9 +4,9 @@
 REMOTE_HOST="80.124.71.136"
 REMOTE_PORT="12829"
 REMOTE_USER="root"
-PRIVATE_KEY_FILE="~/gpu_ssh/gpu" # Update this to the actual path of your private key
-REMOTE_DIR="/workspace/output/*" # This will copy everything in the output directory
-LOCAL_DIR="." # Current local directory
+PRIVATE_KEY_FILE="gpu" 
+REMOTE_DIR="/workspace/lora/output/*" 
+LOCAL_DIR="."
 
 # Copy files from remote server to local directory
 scp -i "$PRIVATE_KEY_FILE" -P "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
