29ae4fba27ecc71ec7e1d8c0b706f17870d8281b
willparker-and <william.parker@and.digital>
2023-11-23 09:58:03 +0000

    Change to use base model

--- eval.py
+++ eval.py
@@ -2,17 +2,11 @@
 import transformers as t
 import torch
 import peft
-import time
+import config
 #%%
 tokenizer = t.AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
 base_model = t.AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf", load_in_8bit=True, torch_dtype=torch.float16)
 tokenizer.pad_token_id = 0
-#%%
-# config = peft.LoraConfig(r=8, lora_alpha=16, target_modules=["q_proj", "v_proj"], lora_dropout=0.005, bias="none", task_type="CAUSAL_LM")
-# model = peft.get_peft_model(model, config)
-# peft.set_peft_model_state_dict(model, model.from_pretrained("checkpoint-800"))
-config = peft.PeftConfig.from_pretrained("./output/checkpoint-800")
-model = peft.PeftModel.from_pretrained(base_model, "./output/checkpoint-800")
 #%%
 with open('./prompt.txt', 'r') as file:
   TEMPLATE = file.read()
@@ -20,5 +14,12 @@ QUESTION = "How many teachers are earning less than £40000"
 CONTEXT = "CREATE TABLE teacher (salary INTEGER)"
 prompt = TEMPLATE.format(question=QUESTION,context=CONTEXT)
 #%%
-pipe = t.pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=500)
+if not config.BASE_MODEL:
+  config = peft.PeftConfig.from_pretrained("./output/checkpoint-800")
+  model = peft.PeftModel.from_pretrained(base_model, "./output/checkpoint-800")
+  pipe = t.pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=500)
+else: 
+  pipe = t.pipeline(task="text-generation", model=base_model, tokenizer=tokenizer, max_length=500)
+
+#%%
 print("pipe(prompt)", pipe(prompt))
