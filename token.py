1700f0567b8fbe668563313cc6a88a0b30faea28
Peter Holdsworth <peter.holdsworth@and.digital>
2023-11-23 09:58:02 +0000

    Add sql keywords as special tokens

---
+++ token.py
@@ -0,0 +1,33 @@
+import transformers
+
+tokenizer = transformers.AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
+
+tokenizer.add_special_tokens({'additional_special_tokens':[
+ "CREATE",
+ "TABLE",
+ "INTEGER",
+ "SELECT",
+ "COUNT(*)",
+ "COUNT",
+ "FROM",
+ "WHERE",
+ "VARCHAR",
+ "ORDER",
+ "MAX",
+ "MIN",
+ "AVG",
+ "BETWEEN",
+ "DISTINCT",
+ "JOIN",
+ "AS",
+ "ON",
+ "GROUP",
+ "BY",
+ "HAVING",
+ "LIMIT",
+ "ASC",
+ "DESC",
+ "LIKE",
+ "INTERSECT",
+ "EXCEPT"                              
+]})
