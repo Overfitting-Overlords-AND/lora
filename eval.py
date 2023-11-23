acb647c4ad9a83232f67ea2169bdaabd92d44e70
willparker-and <william.parker@and.digital>
2023-11-23 15:12:40 +0000

    Updates

--- eval.py
+++ eval.py
@@ -7,11 +7,14 @@ import config
 #%%
 tokenizer.pad_token_id = 0
 base_model = t.AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf", load_in_8bit=True, torch_dtype=torch.float16)
+base_model.resize_token_embeddings(len(tokenizer))
 #%%
 with open('./prompt.txt', 'r') as file:
   TEMPLATE = file.read()
-QUESTION = "How many teachers are earning less than £40000"
-CONTEXT = "CREATE TABLE teacher (salary INTEGER)"
+QUESTION = "How many teachers does the student named CHRISSY NABOZNY have?"
+CONTEXT = "CREATE TABLE teachers (classroom VARCHAR); CREATE TABLE list (classroom VARCHAR, firstname VARCHAR, lastname VARCHAR)"
+# SELECT COUNT(*) FROM list AS T1 JOIN teachers AS T2 ON T1.classroom = T2.classroom WHERE T1.firstname = “CHRISSY” AND T1.lastname = “NABOZNY”
+
 prompt = TEMPLATE.format(question=QUESTION,context=CONTEXT)
 #%%
 if not config.BASE_MODEL:
