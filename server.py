d9077e3b7c73bba295d3d0889b69581e731cf94a
Peter Holdsworth <peter.holdsworth@and.digital>
2023-11-23 16:51:10 +0000

    Added swever.py

---
+++ server.py
@@ -0,0 +1,40 @@
+import fastapi
+from tokens import tokenizer
+import transformers as t
+import torch
+import peft
+
+app = fastapi.FastAPI()
+
+# run with - uvicorn server:app --host 0.0.0.0 --port 8080 --reload
+@app.on_event("startup")
+async def startup_event():
+  print("Starting Up")
+  app.state.tokenizer = tokenizer
+  app.state.tokenizer.pad_token_id = 0
+  app.state.base_model = t.AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf", load_in_8bit=True, torch_dtype=torch.float16)
+  app.state.model = peft.PeftModel.from_pretrained(app.state.base_model, "./output/checkpoint-800")
+  app.state.pipe = t.pipeline(task="text-generation", model=app.state.model, tokenizer=app.state.tokenizer, max_length=500)
+  with open('./prompt.txt', 'r') as file:
+    app.state.template = file.read()
+
+@app.on_event("shutdown")
+async def startup_event():
+  print("Shutting down")
+
+@app.get("/")
+def on_root():
+  return { "message": "Hello App" }
+
+@app.post("/text_to_sql")
+async def text_to_sql(request: fastapi.Request):
+  payload = await request.json()
+  prompt = app.state.template.format(question=payload["QUESTION"],context=payload["CONTEXT"])
+  answer = app.state.pipe(prompt)
+  print("Input question: ", payload["question"])
+  print("Input context: ", payload["context"])
+  return answer
+
+
+
+
