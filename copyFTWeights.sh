e44ad729e91361429eaf0f3a1ef3b71e2483368b
willparker-and <william.parker@and.digital>
2023-11-23 16:57:28 +0000

    *

--- copyFTWeights.sh
+++ copyFTWeights.sh
@@ -4,9 +4,9 @@
 REMOTE_HOST="80.124.71.136"
 REMOTE_PORT="12829"
 REMOTE_USER="root"
-PRIVATE_KEY_FILE="gpu" 
+PRIVATE_KEY_FILE="~/.ssh/gpu" 
 REMOTE_DIR="/workspace/lora/output/*" 
 LOCAL_DIR="."
 
 # Copy files from remote server to local directory
-scp -i "$PRIVATE_KEY_FILE" -P "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
+scp -r -i "$PRIVATE_KEY_FILE" -P "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
