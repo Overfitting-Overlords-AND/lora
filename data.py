419da658d9735b023145e6006aba1c52288421ba
Peter Holdsworth <peter.holdsworth@and.digital>
2023-11-24 09:48:55 +0000

    pad on right

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ class DatasetReader(Dataset):
       self.template = file.read()
     self.tokenizer = tr
     self.tokenizer.pad_token_id = 0
-    self.tokenizer.padding_side = "left"
+    self.tokenizer.padding_side = "right"
     self.ds = ds
     self.ds = self.ds.map(self.prompt, remove_columns=["question", "context", "answer"], load_from_cache_file=False, num_proc=8)
     self.ds = self.ds.map(self.tokenize, remove_columns=["prompt"], load_from_cache_file=False, num_proc=8)
