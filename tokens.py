b9e9d4dbd24b19d6d6af8f951c9d10eb6b02afe7
willparker-and <william.parker@and.digital>
2023-11-24 16:32:36 +0000

    Temp change

--- tokens.py
+++ tokens.py
@@ -3,33 +3,33 @@ import config
 
 tokenizer = transformers.AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
 
-# if not config.BASE_MODEL:
-#     tokenizer.add_special_tokens({'additional_special_tokens':[
-#     "CREATE",
-#     "TABLE",
-#     "INTEGER",
-#     "SELECT",
-#     "COUNT(*)",
-#     "COUNT",
-#     "FROM",
-#     "WHERE",
-#     "VARCHAR",
-#     "ORDER",
-#     "MAX",
-#     "MIN",
-#     "AVG",
-#     "BETWEEN",
-#     "DISTINCT",
-#     "JOIN",
-#     "AS",
-#     "ON",
-#     "GROUP",
-#     "BY",
-#     "HAVING",
-#     "LIMIT",
-#     "ASC",
-#     "DESC",
-#     "LIKE",
-#     "INTERSECT",
-#     "EXCEPT"                              
-#     ]})
+if not config.BASE_MODEL:
+    tokenizer.add_special_tokens({'additional_special_tokens':[
+    "CREATE",
+    "TABLE",
+    "INTEGER",
+    "SELECT",
+    "COUNT(*)",
+    "COUNT",
+    "FROM",
+    "WHERE",
+    "VARCHAR",
+    "ORDER",
+    "MAX",
+    "MIN",
+    "AVG",
+    "BETWEEN",
+    "DISTINCT",
+    "JOIN",
+    "AS",
+    "ON",
+    "GROUP",
+    "BY",
+    "HAVING",
+    "LIMIT",
+    "ASC",
+    "DESC",
+    "LIKE",
+    "INTERSECT",
+    "EXCEPT"                              
+    ]})
